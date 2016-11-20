c9a51ff011e0b7163e61515671b0d869cecd133d
Kirankumar Adam <kiranadam@gmail.com>
2016-11-20 16:53:39 +0530

    Add files via upload

--- src/ChanVese.cpp
+++ src/ChanVese.cpp
@@ -53,10 +53,10 @@ vector<double> ChanVese :: Heavyside_avg(Mat& src, Mat& phi, double epsilon)
 		#pragma omp parallel for
 		for(unsigned j=0; j<src.cols; j++)
 		{
-			double H_phi = 0.5*(1+(2.0/CV_PI)*atan2((double)phi.at<uchar>(i,j),epsilon));
-			c1_num += (double)src.at<uchar>(i,j)*H_phi;
+			double H_phi = 0.5*(1+(2.0/CV_PI)*atan2((double)phi.ptr<uchar>(i)[j],epsilon));
+			c1_num += (double)src.ptr<uchar>(i)[j]*H_phi;
 			c1_den += H_phi;
-			c2_num += (double)src.at<uchar>(i,j)*(1-H_phi);
+			c2_num += (double)src.ptr<uchar>(i)[j]*(1-H_phi);
 			c2_den += 1-H_phi;
 		}
 	}
@@ -81,7 +81,7 @@ Mat ChanVese :: delta_phi(Mat& phi)
 		#pragma omp parallel for
 		for(unsigned j=0; j<phi.cols; j++)
 		{
-			del_phi.at<double>(i,j) = (1/CV_PI)*( h/( pow(h,2) + pow((double)phi.at<uchar>(i,j),2) ));
+			del_phi.ptr<double>(i)[j] = (1/CV_PI)*(h/(pow(h,2) + pow((double)phi.ptr<uchar>(i)[j],2)));
 		}
 	}
 
@@ -103,7 +103,7 @@ double ChanVese :: perimeter_length(Mat& phi, Mat& del_phi)
 		#pragma omp parallel for
 		for(unsigned j=0; j<phi.cols; j++)
 		{
-			L += del_phi.at<double>(i,j) * abs_nabla_phi.at<double>(i,j);
+			L += del_phi.ptr<double>(i)[j] * abs_nabla_phi.ptr<double>(i)[j];
 		}
 	}
 
@@ -118,17 +118,17 @@ vector<double> ChanVese :: CV_coefficients(Mat& phi, Mat& del_phi, double L, uns
 	double C[4];
 	double zero_nullifier = 0.0001; // constant to avoid divide by zero case
 
-	C[0] = 1/sqrt(pow((phi.at<double>(i+1,j)-phi.at<double>(i,j)),2) + pow((phi.at<double>(i,j+1)-phi.at<double>(i,j-1)),2)/4 + zero_nullifier);
-	C[1] = 1/sqrt(pow((phi.at<double>(i,j)-phi.at<double>(i-1,j)),2) + pow((phi.at<double>(i-1,j+1)-phi.at<double>(i-1,j-1)),2)/4 + zero_nullifier);
-	C[2] = 1/sqrt(pow((phi.at<double>(i+1,j)-phi.at<double>(i-1,j)),2)/4 + pow((phi.at<double>(i,j+1)-phi.at<double>(i,j)),2) + zero_nullifier);
-	C[3] = 1/sqrt(pow((phi.at<double>(i+1,j-1)-phi.at<double>(i-1,j-1)),2)/4 + pow((phi.at<double>(i,j)-phi.at<double>(i,j-1)),2) + zero_nullifier); 
+	C[0] = 1/sqrt(pow((phi.ptr<double>(i+1)[j]-phi.ptr<double>(i)[j]),2) + pow((phi.ptr<double>(i)[j+1]-phi.ptr<double>(i)[j-1]),2)/4 + zero_nullifier);
+	C[1] = 1/sqrt(pow((phi.ptr<double>(i)[j]-phi.ptr<double>(i-1)[j]),2) + pow((phi.ptr<double>(i-1)[j+1]-phi.ptr<double>(i-1)[j-1]),2)/4 + zero_nullifier);
+	C[2] = 1/sqrt(pow((phi.ptr<double>(i+1)[j]-phi.ptr<double>(i-1)[j]),2)/4 + pow((phi.ptr<double>(i)[j+1]-phi.ptr<double>(i)[j]),2) + zero_nullifier);
+	C[3] = 1/sqrt(pow((phi.ptr<double>(i+1)[j-1]-phi.ptr<double>(i-1)[j-1]),2)/4 + pow((phi.ptr<double>(i)[j]-phi.ptr<double>(i)[j-1]),2) + zero_nullifier); 
 
-	double F_den = h + dt*del_phi.at<double>(i,j) * mu * (p*pow(L,p-1)) * (C[0]+C[1]+C[2]+C[3]);
+	double F_den = h + dt*del_phi.ptr<double>(i)[j] * mu * (p*pow(L,p-1)) * (C[0]+C[1]+C[2]+C[3]);
 	double F_num = 0.0;
 
 	for(unsigned k=0; k<4; k++)
 	{
-		F_num = (dt * del_phi.at<double>(i,j) * mu *(p*pow(L,p-1)) * C[k]) / F_den;
+		F_num = (dt * del_phi.ptr<double>(i)[j] * mu *(p*pow(L,p-1)) * C[k]) / F_den;
 		F.push_back(F_num);
 	}
 
@@ -176,35 +176,29 @@ Mat ChanVese :: ChanVese_segment(Mat& src, Mat& phi0, unsigned iteration)
 				{
 					F = CV_coefficients( phi, del_phi, L, i, j);
 				
-					double Pi_j = phi.at<double>(i,j) + dt * del_phi.at<double>(i,j)*(nu + lambda1*pow(((double)src.at<uchar>(i,j)-c[0]),2) + lambda2*pow(((double)src.at<uchar>(i,j)-c[1]),2) );  
+					double Pi_j = phi.ptr<double>(i)[j] + dt * del_phi.ptr<double>(i)[j]*(nu + lambda1*pow(((double)src.ptr<uchar>(i)[j]-c[0]),2) + lambda2*pow(((double)src.ptr<uchar>(i)[j]-c[1]),2) );  
 					//Update phi		
-					new_phi.at<double>(i,j) = F[0]*phi.at<double>(i+1,j) + F[1]*phi.at<double>(i-1,j) + F[2]*phi.at<double>(i,j+1) + F[3]*phi.at<double>(i,j-1) + F[4]*Pi_j;
+					new_phi.ptr<double>(i)[j] = F[0]*phi.ptr<double>(i+1)[j] + F[1]*phi.ptr<double>(i-1)[j] + F[2]*phi.ptr<double>(i)[j+1] + F[3]*phi.ptr<double>(i)[j-1] + F[4]*Pi_j;
 				}
 			}
 
 			// Border update 
 			for(unsigned i=0; i < src.rows; i++)
 			{
-				new_phi.at<double>(i,0) = new_phi.at<double>(i,1);
-				new_phi.at<double>(i,src.cols-1) = new_phi.at<double>(i,src.cols-2);
-
+				new_phi.ptr<double>(i)[0] = new_phi.ptr<double>(i)[1];
+				new_phi.ptr<double>(i)[src.cols-1] = new_phi.ptr<double>(i)[src.cols-2];
 			}
 
 			for(unsigned j=0; j < src.cols; j++)
 			{
-				new_phi.at<double>(0,j) = new_phi.at<double>(1,j);
-				new_phi.at<double>(src.rows-1,j) = new_phi.at<double>(src.rows-2,j);
-
+				new_phi.ptr<double>(0)[j] = new_phi.ptr<double>(1)[j];
+				new_phi.ptr<double>(src.rows-1)[j] = new_phi.ptr<double>(src.rows-2)[j];
 			}
 			
 			new_phi = ReinitPhi(new_phi,100);
-			
 			phi = new_phi.clone();
-
 		}
 	}
-
-
 	return new_phi;
 }
 
@@ -235,27 +229,26 @@ Mat ChanVese :: ReinitPhi(Mat& phi, unsigned iterations)
     		{
       			for(unsigned j = 1; j < phi.cols-1; j++)
      			{
-        			a = ( phi.at<double>(i,j) - phi.at<double>(i-1,j) )/h;
-        			b = ( phi.at<double>(i+1,j) - phi.at<double>(i,j) )/h;
-        			c = ( phi.at<double>(i,j) - phi.at<double>(i,j-1) )/h;
-        			d = ( phi.at<double>(i,j+1) - phi.at<double>(i,j) )/h;
+        				a = (phi.ptr<double>(i)[j] - phi.ptr<double>(i-1)[j])/h;
+        				b = (phi.ptr<double>(i+1)[j] - phi.ptr<double>(i)[j])/h;
+        				c = (phi.ptr<double>(i)[j] - phi.ptr<double>(i)[j-1])/h;
+        				d = (phi.ptr<double>(i)[j+1] - phi.ptr<double>(i)[j])/h;
 
-        			if(phi.at<double>(i,j) > 0)
+        				if(phi.ptr<double>(i)[j] > 0)
 				{
           				G = sqrt(max(pow(max(a,0.0),2),pow(min(b,0.0),2)) + max(pow(max(c,0.0),2),pow(min(d,0.0),2))) - 1.0;
 				}
-        			else if(phi.at<double>(i,j) < 0)
+        				else if(phi.ptr<double>(i)[j] < 0)
 				{
           				G = sqrt(max(pow(min(a,0.0),2),pow(max(b,0.0),2)) + max(pow(min(c,0.0),2),pow(max(d,0.0),2))) - 1.0;
 				}
-        			else
+        				else
 				{
           				G = 0;
 				}
 
-        			x = (phi.at<double>(i,j) >= 0)?(1.0):(-1.0);
-
-        			psi.at<double>(i,j) = psi.at<double>(i,j) - dt*x*G;
+        				x = (phi.ptr<double>(i)[j]= 0)?(1.0):(-1.0);
+        				psi.ptr<double>(i)[j] = psi.ptr<double>(i)[j] - dt*x*G;
       			}
     		}
 
@@ -263,15 +256,16 @@ Mat ChanVese :: ReinitPhi(Mat& phi, unsigned iterations)
     		Q = 0.0;
 		M = 0.0;
 
-    		for (unsigned i = 0; i < phi.rows; i++)
+    		for(unsigned i = 0; i < phi.rows; i++)
     		{
-      			for (unsigned j = 0; j < phi.cols; j++)
+			#pragma omp parallel for
+      			for(unsigned j = 0; j < phi.cols; j++)
       			{
-        			if(abs(old_psi.at<double>(i,j)) <= h)
-        			{
+        				if(abs(old_psi.ptr<double>(i)[j]) <= h)
+        				{
           				M++;
-          				Q += abs(old_psi.at<double>(i,j) - psi.at<double>(i,j));
-        			}
+          				Q += abs(old_psi.ptr<double>(i)[j] - psi.ptr<double>(i)[j]);
+        				}
       			}
     		}
 
@@ -287,10 +281,73 @@ Mat ChanVese :: ReinitPhi(Mat& phi, unsigned iterations)
     		if (Q < dt*h*h)
     		{
       			flag = true;
-      
     		}
     
   	}
 
 	return psi;
+}
+
+
+
+Mat ChanVese :: edge_representation(Mat& phi)
+{
+  
+	Mat edge = Mat::zeros(phi.rows,phi.cols,CV_8UC1);
+
+  	for(unsigned i = 0; i < phi.rows; i++)
+  	{
+    		for(unsigned j = 0; j < phi.cols; j++)
+    		{
+      			// Checking interior pixels to avoid bounds checking
+      			if (i > 0 && i < phi.rows-1 && j > 0 && j < phi.cols-1)
+      			{
+        				if(phi.ptr<double>(i)[j] == 0)
+        				{
+          				if(phi.ptr<double>(i-1)[j-1] != 0 || phi.ptr<double>(i-1)[j] != 0 ||
+           				   phi.ptr<double>(i-1)[j+1] != 0 || phi.ptr<double>(i)[j-1] != 0 ||
+           				   phi.ptr<double>(i)[j+1] != 0 || phi.ptr<double>(i+1)[j-1] != 0 ||
+           				   phi.ptr<double>(i+1)[j] != 0 || phi.ptr<double>(i+1)[j+1] != 0 )
+					{
+             					edge.ptr<uchar>(i)[j] = (uchar)255;
+           				}
+        				}
+        				else
+        				{
+          				if(abs(phi.ptr<double>(i)[j]) < abs(phi.ptr<double>(i-1)[j-1]) && (phi.ptr<double>(i)[j]>0) != (phi.ptr<double>(i-1)[j-1]>0))
+					{
+             					edge.ptr<uchar>(i)[j] = (uchar)255;
+					}
+     					else if(abs(phi.ptr<double>(i)[j]) < abs(phi.ptr<double>(i-1)[j]) && (phi.ptr<double>(i)[j]>0) != (phi.ptr<double>(i-1)[j]>0))
+					{
+             					edge.ptr<uchar>(i)[j] = (uchar)255;
+					}
+     					else if(abs(phi.ptr<double>(i)[j]) < abs(phi.ptr<double>(i-1)[j+1]) && (phi.ptr<double>(i)[j]>0) != (phi.ptr<double>(i-1)[j+1]>0))
+					{
+             					edge.ptr<uchar>(i)[j] = (uchar)255;
+					}
+     					else if(abs(phi.ptr<double>(i)[j]) < abs(phi.ptr<double>(i)[j-1]) && (phi.ptr<double>(i)[j]>0) != (phi.ptr<double>(i)[j-1]>0))
+					{
+             					edge.ptr<uchar>(i)[j] = (uchar)255;
+					}
+     					else if(abs(phi.ptr<double>(i)[j]) < abs(phi.ptr<double>(i)[j+1]) && (phi.ptr<double>(i)[j]>0) != (phi.ptr<double>(i)[j+1]>0))
+					{
+       						edge.ptr<uchar>(i)[j] = (uchar)255;
+					}
+     					else if(abs(phi.ptr<double>(i)[j]) < abs(phi.ptr<double>(i+1)[j-1]) && (phi.ptr<double>(i)[j]>0) != (phi.ptr<double>(i+1)[j-1]>0))
+					{
+             					edge.ptr<uchar>(i)[j] = (uchar)255;
+					}
+     					else if(abs(phi.ptr<double>(i)[j]) < abs(phi.ptr<double>(i+1)[j]) && (phi.ptr<double>(i)[j]>0) != (phi.ptr<double>(i+1)[j]>0))
+					{
+       						edge.ptr<uchar>(i)[j] = (uchar)255;
+					}
+     					else if(abs(phi.ptr<double>(i)[j]) < abs(phi.ptr<double>(i+1)[j+1]) && (phi.ptr<double>(i)[j]>0) != (phi.ptr<double>(i+1)[j+1]>0))
+       					{  
+						edge.ptr<uchar>(i)[j] = (uchar)255;
+        					}
+      				}
+    			}
+  		}
+	}
 }
--- src/test_main.cpp
+++ src/test_main.cpp
@@ -1,4 +1,3 @@
-
 #include "ChanVese.hpp"
 #include "opencv2/opencv.hpp"
 #include <iostream>
@@ -10,14 +9,22 @@ using namespace cv;
 
 int main(int, char**)
 {
-	Mat src = imread("../../ChanVese/M-78B-1400_2.jpg",0); // input gray scale image
+	Mat src = imread("../../ChanVese/img_test.jpg",0); // input gray scale image
   
 	Mat phi0 = Mat::zeros(src.rows,src.cols,CV_64FC1);
   	Mat phi, edge;
 
+	double lambda1 = 1.02;
+	double lambda2 = 1.0;
+	int p = 1;
+	double mu = 70;
+	double nu = 1900;
+	double h = 1;
+	double dt = 0.1;
+
 
   	// Create object of ChanVese class
-	ChanVese c;
+	ChanVese c(lambda1,lambda2,p,mu,nu,h,dt);
 
   	// Set up initial circular contour 
   	double x;
@@ -29,15 +36,47 @@ int main(int, char**)
     		{
       			x = double(i) - src.rows/2.0;
       			y = double(j) - src.cols/2.0;
-      			phi0.at<double>(i,j) = 900.0/(900.0 + x*x + y*y) - 0.5;
+      			phi0.ptr<double>(i)[j] = 900.0/(900.0 + x*x + y*y) - 0.5;
     		}
   	}
 
   	// Segmentation
-  	phi = c.ChanVese_segment(src,phi0, 5);
-	imshow("Phi",phi);
+  	phi = c.ChanVese_segment(src,phi0, 2);
+	
+	Mat U_phi;
+	phi.convertTo(U_phi,CV_8UC1);
+
+	imwrite("../../ChanVese/output.jpg",U_phi);
+	imshow("Phi",U_phi);
 	waitKey(-1);
 	cout<<"Done Segmentation"<<endl;
   
+	/*
+	// edges
+  	edge = c.edge_representation(phi);
+	imshow("Edges",edge);
+
+  	imwrite("CV_Egde.jpg",edge);
+
+	Mat output = Mat::zeros(src.rows,src.cols,CV_8UC1);
+  
+  	for(unsigned i = 0; i < src.rows; i++)
+  	{
+    		for(unsigned j = 0; j < src.cols; j++)
+    		{
+      			if(phi.at<double>(i,j) >= 0)
+			{
+        			output.at<uchar>(i,j) = (uchar)0;
+			}
+      			else
+			{
+        			output.at<uchar>(i,j) = (uchar)255;
+			}
+    		}
+  	}
+
+  	imwrite("CV_Regions.pgm",output);
+	
+	*/
 	return 0;
 }
